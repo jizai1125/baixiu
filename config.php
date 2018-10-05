221536711e2d607f58da483804061af1b7188864
Fighter1125 <1414589221@qq.com>
2018-10-05 16:46:42 +0800

    项目配置文件

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * 数据库主机
+ */
+define('CHEN_DB_HOST','127.0.0.1');
+/**
+ * 数据库用户名
+ */
+define('CHEN_DB_USER','root');
+/**
+ * 数据库密码
+ */
+define('CHEN_DB_PASSWORD','199611');
+/**
+ * 数据库名称
+ */
+define('CHEN_DB_NAME','baixiu');
+/**
+ * 当前路径 D:\Myweb\chen\baixiu\admin\inc
+ */
+define('ROOT_DIR',dirname(__FILE__));
