c7f51cde989f08b87386d2ad0d67680524ac9130
Fighter1125 <1414589221@qq.com>
2018-10-05 20:31:15 +0800

    顶部菜单栏

--- admin/inc/navbar.php
+++ admin/inc/navbar.php
@@ -1,3 +1,7 @@
+<?php
+require_once dirname(__FILE__).'/../../functions.php';
+chen_get_current_user();
+?>
 <nav class="navbar">
     <button class="btn btn-default navbar-btn fa fa-bars"></button>
     <ul class="nav navbar-nav navbar-right">
